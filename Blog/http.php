bc0c33b334cd20f8cdcd808dccd58ea419c9e738
nebeltag <nebeltag@mail.ru>
2023-05-09 11:08:29 +0300

    homework_4: add class Request

---
+++ Blog/http.php
@@ -0,0 +1,16 @@
+<?php
+
+use GeekBrains\LevelTwo\Http\Request;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$request = new Request($_GET, $_SERVER);
+
+$path = $request->path();
+$name = $request->query('Cookie');
+$header = $request->header('cookie');
+
+
+echo "$path\n";
+echo "$name\n";
+echo "$header";
